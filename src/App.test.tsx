d06a2b779d45842e3d95262b4ec19d3a1d0ebc72
Willem Verbuyst <willemverbuyst@gmail.com>
2024-12-29 05:03:42 +0100

    Use before each hook in tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,35 +1,32 @@
 import "@testing-library/jest-dom";
 import { fireEvent, render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
-
+import { beforeEach, describe, expect, it } from "vitest";
 import App from "./App";
 
 describe("App", () => {
-  it("should render with the title visible", () => {
+  let usernameInput: HTMLInputElement;
+  let passwordInput: HTMLInputElement;
+  let button: HTMLButtonElement;
+
+  beforeEach(() => {
     render(<App />);
 
+    usernameInput = screen.getByLabelText(/username/i) as HTMLInputElement;
+    passwordInput = screen.getByLabelText(/password/i) as HTMLInputElement;
+    button = screen.getByRole("button", { name: /submit/i });
+  });
+
+  it("should render app with the title visible", () => {
     expect(screen.getByText(/login/i)).toBeDefined();
   });
 
   it("should render with 2 input fields and a button", () => {
-    render(<App />);
-
-    expect(screen.getByLabelText(/username/i)).toBeDefined();
-    expect(screen.getByLabelText(/password/i)).toBeDefined();
-    expect(screen.getByRole("button", { name: /submit/i })).toBeDefined();
+    expect(usernameInput).toBeDefined();
+    expect(passwordInput).toBeDefined();
+    expect(button).toBeDefined();
   });
 
   it("should render a welcome message after submitting form correctly", () => {
-    render(<App />);
-
-    const usernameInput = screen.getByLabelText(
-      /username/i
-    ) as HTMLInputElement;
-    const passwordInput = screen.getByLabelText(
-      /password/i
-    ) as HTMLInputElement;
-    const button = screen.getByRole("button", { name: /submit/i });
-
     fireEvent.change(usernameInput, { target: { value: "John Doe" } });
     fireEvent.change(passwordInput, { target: { value: "any_password_123" } });
     fireEvent.click(button);
@@ -39,125 +36,73 @@ describe("App", () => {
   });
 
   it("should render an error message when password is missing", () => {
-    render(<App />);
-
-    const usernameInput = screen.getByLabelText(
-      /username/i
-    ) as HTMLInputElement;
-    const passwordInput = screen.getByLabelText(
-      /password/i
-    ) as HTMLInputElement;
-    const button = screen.getByRole("button", { name: /submit/i });
-
     fireEvent.change(usernameInput, { target: { value: "John Doe" } });
     fireEvent.click(button);
 
     const errorMessage = screen.getByText(/password is missing/i);
+
     expect(errorMessage).toBeDefined();
     expect(errorMessage).toHaveStyle({ color: "rgb(255, 0, 0)" });
     expect(passwordInput.matches(":focus")).toBe(true);
   });
 
   it("should render an error message when username is missing", () => {
-    render(<App />);
-
-    const usernameInput = screen.getByLabelText(
-      /username/i
-    ) as HTMLInputElement;
-    const passwordInput = screen.getByLabelText(
-      /password/i
-    ) as HTMLInputElement;
-
-    const button = screen.getByRole("button", { name: /submit/i });
-
     fireEvent.change(passwordInput, { target: { value: "any_password_123" } });
     fireEvent.click(button);
 
     const errorMessage = screen.getByText(/username is missing/i);
+
     expect(errorMessage).toBeDefined();
     expect(errorMessage).toHaveStyle({ color: "rgb(255, 0, 0)" });
     expect(usernameInput.matches(":focus")).toBe(true);
   });
 
   it("should render an error message when username is an empty string", () => {
-    render(<App />);
-
-    const usernameInput = screen.getByLabelText(
-      /username/i
-    ) as HTMLInputElement;
-    const passwordInput = screen.getByLabelText(
-      /password/i
-    ) as HTMLInputElement;
-
-    const button = screen.getByRole("button", { name: /submit/i });
-
     fireEvent.change(usernameInput, { target: { value: " " } });
     fireEvent.change(passwordInput, { target: { value: "any_password_123" } });
     fireEvent.click(button);
 
     const errorMessage = screen.getByText(/username is missing/i);
+
     expect(errorMessage).toBeDefined();
     expect(errorMessage).toHaveStyle({ color: "rgb(255, 0, 0)" });
     expect(usernameInput.matches(":focus")).toBe(true);
   });
 
   it("should render an error message when password is an empty string", () => {
-    render(<App />);
-
-    const usernameInput = screen.getByLabelText(
-      /username/i
-    ) as HTMLInputElement;
-    const passwordInput = screen.getByLabelText(
-      /password/i
-    ) as HTMLInputElement;
-
-    const button = screen.getByRole("button", { name: /submit/i });
-
     fireEvent.change(usernameInput, { target: { value: "John Doe" } });
     fireEvent.change(passwordInput, { target: { value: " " } });
     fireEvent.click(button);
 
     const errorMessage = screen.getByText(/password is missing/i);
+
     expect(errorMessage).toBeDefined();
     expect(errorMessage).toHaveStyle({ color: "rgb(255, 0, 0)" });
     expect(usernameInput.matches(":focus")).toBe(true);
   });
 
-  it("should render error messages when values are missing", () => {
-    render(<App />);
-
-    const usernameInput = screen.getByLabelText(
-      /username/i
-    ) as HTMLInputElement;
-    const button = screen.getByRole("button", { name: /submit/i });
-
+  it("should render error messages when both values are missing", () => {
     fireEvent.click(button);
 
     const errorMessagePassword = screen.getByText(/password is missing/i);
+
     expect(errorMessagePassword).toBeDefined();
     expect(errorMessagePassword).toHaveStyle({ color: "rgb(255, 0, 0)" });
+
     const errorMessageUserName = screen.getByText(/username is missing/i);
+
     expect(errorMessageUserName).toBeDefined();
     expect(errorMessageUserName).toHaveStyle({ color: "rgb(255, 0, 0)" });
     expect(usernameInput.matches(":focus")).toBe(true);
   });
 
   it("should navigate back to empty form", () => {
-    render(<App />);
-
-    const usernameInput = screen.getByLabelText(
-      /username/i
-    ) as HTMLInputElement;
-    const passwordInput = screen.getByLabelText(
-      /password/i
-    ) as HTMLInputElement;
-    const button = screen.getByRole("button", { name: /submit/i });
-
     fireEvent.change(usernameInput, { target: { value: "John Doe" } });
     fireEvent.change(passwordInput, { target: { value: "any_password_123" } });
     fireEvent.click(button);
 
     const logoutBtn = screen.getByRole("button", { name: /log out/i });
+
     fireEvent.click(logoutBtn);
 
     expect(screen.getByText(/login/i)).toBeDefined();
